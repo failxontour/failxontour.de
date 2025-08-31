dd8a6a64b0c5e3ee85f6f1235db73bf0135b7d07
failxontour <228637610+failxontour@users.noreply.github.com>
2025-08-31 17:14:31 +0200

    added composer & php-parser added recipe for validation against Lighthouse CI assertions new .json file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-public',
+        'version' => 'dev-public',
+        'reference' => '4922c978bbde4e2a867503b8a17621b3092a0ba1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-public',
+            'version' => 'dev-public',
+            'reference' => '4922c978bbde4e2a867503b8a17621b3092a0ba1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nikic/php-parser' => array(
+            'pretty_version' => 'v5.6.1',
+            'version' => '5.6.1.0',
+            'reference' => 'f103601b29efebd7ff4a1ca7b3eeea9e3336a2a2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nikic/php-parser',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
