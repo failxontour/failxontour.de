9a782c53e93e052e21170fe21cc4d34a8edf6924
failxontour <228637610+failxontour@users.noreply.github.com>
2025-10-29 15:39:26 +0100

    .gitignore change and skipping excluded dirs in sitemap

--- sitemap.php
+++ sitemap.php
@@ -1,11 +1,19 @@
 <?php
-function recurse ($dir) {
-  $files = array_diff (scandir ($dir), ['.', '..']);
+function recurse($dir) {
+  $files = array_diff(scandir($dir), ['.', '..']);
+  
+  // Verzeichnisse, die ausgeschlossen werden sollen
+  $excludedDirs = ['.vscode', '.github', '.git'];
+  
   foreach ($files as $file) {
+    // Überprüfen, ob das Verzeichnis ausgeschlossen werden soll
+    if (in_array($file, $excludedDirs)) {
+      continue; // Überspringe dieses Verzeichnis
+    }
 
-    if (is_dir ($dir.'/'.$file)) {
+    if (is_dir($dir.'/'.$file)) {
       echo '<li><a href="'.htmlspecialchars($dir.'/'.$file).'"><b>'.htmlspecialchars($file).'</b></a><ul>'.PHP_EOL;
-      recurse ($dir.'/'.$file);
+      recurse($dir.'/'.$file);
       echo '</ul></li>'.PHP_EOL;
     } elseif (pathinfo($dir.'/'.$file, PATHINFO_EXTENSION) == 'html') {
       preg_match("'<h1>(.*?)</h1>'si", file_get_contents($dir.'/'.$file), $heading);
