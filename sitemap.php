e2ba3a29593dadc8809e4c072bfaa68e73e62fee
failxontour <228637610+failxontour@users.noreply.github.com>
2025-10-29 16:34:31 +0100

    no more lib!

--- sitemap.php
+++ sitemap.php
@@ -33,7 +33,7 @@ function recurse($dir, &$links) {
   $files = array_diff(scandir($dir), ['.', '..']);
   
   // Verzeichnisse, die ausgeschlossen werden sollen
-  $excludedDirs = ['.vscode', '.github', '.git', 'js', 'css', 'downloads', 'images', 'hashover', 'svgs', 'audio', 'videos/lib'];
+  $excludedDirs = ['.vscode', '.github', '.git', 'js', 'css', 'downloads', 'images', 'hashover', 'svgs', 'audio', 'lib'];
 
   foreach ($files as $file) {
     // Überprüfen, ob das Verzeichnis ausgeschlossen werden soll
