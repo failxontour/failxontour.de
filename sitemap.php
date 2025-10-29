eaa09dab534d9a722b10a313e6c0358d981397d9
failxontour <228637610+failxontour@users.noreply.github.com>
2025-10-29 16:40:28 +0100

    hiding videos?

--- sitemap.php
+++ sitemap.php
@@ -33,7 +33,7 @@ function recurse($dir, &$links) {
   $files = array_diff(scandir($dir), ['.', '..']);
   
   // Verzeichnisse, die ausgeschlossen werden sollen
-  $excludedDirs = ['.vscode', '.github', '.git', 'js', 'css', 'downloads', 'images', 'hashover', 'svgs', 'audio', 'lib'];
+  $excludedDirs = ['.vscode', '.github', '.git', 'js', 'css', 'downloads', 'images', 'hashover', 'svgs', 'audio', 'videos'];
 
   foreach ($files as $file) {
     // Überprüfen, ob das Verzeichnis ausgeschlossen werden soll
